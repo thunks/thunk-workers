f05b967cf4b03c31ad86d536292892f8a789cee5
= <=>
2015-09-10 18:20:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var thunk = require('thunks')()
+
+module.exports = function (options) {
+  options = options || {}
+
+  var maxThread = options.maxThread || 1
+  var pending = 0
+  var queue = []
+
+  function flushQueue () {
+    if (pending >= maxThread || !queue.length) return
+    var job = queue.shift()
+    pending++
+
+    thunk.call(job.ctx, job.task)(function (err, res) {
+      pending--
+      flushQueue()
+      if (err) throw err
+      return res
+    })(job.callback)
+  }
+
+  return function (task) {
+    if (typeof task !== 'function') {
+      throw new Error('Task "' + String(task) + '" is not function!')
+    }
+    return thunk.call(this, function (callback) {
+      queue.push(new Job(this, task, callback))
+      flushQueue()
+    })
+  }
+}
+
+function Job (ctx, task, callback) {
+  this.ctx = ctx
+  this.task = task
+  this.callback = callback
+}
